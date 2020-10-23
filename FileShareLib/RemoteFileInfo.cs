fd77000061ff5a78a81ec93998fd536efd6bd29c
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-23 18:38:36 +0300

    Добавление контракта RemoteFileInfo

---
+++ FileShareLib/RemoteFileInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileShareLib
+{
+    [MessageContract]
+    public class RemoteFileInfo : IDisposable
+    {
+        [MessageHeader(MustUnderstand = true)]
+        public string FileName;
+
+        [MessageHeader(MustUnderstand = true)]
+        public long Length;
+
+        [MessageBodyMember(Order = 1)]
+        public System.IO.Stream FileByteStream;
+
+        public void Dispose()
+        {
+            if (FileByteStream != null)
+            {
+                FileByteStream.Close();
+                FileByteStream = null;
+            }
+        }
+    }
+
+}
