632184812ac5a80cf6274e05700158a3c433563a
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-23 18:34:52 +0300

    Добавьте файлы проекта.

---
+++ FileShareLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения о сборке можно задать с помощью следующего 
+// атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+// связанные со сборкой.
+[assembly: AssemblyTitle("FileShareLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("FileShareLib")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Если для ComVisible установить значение false, типы в этой сборке не будут поддерживаться 
+// COM-компонентами.  При необходимости доступа к какому-либо типу в этой сборке 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если данный проект видим для COM
+[assembly: Guid("be78c86d-caf1-4c46-b3af-b07d472ef443")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
