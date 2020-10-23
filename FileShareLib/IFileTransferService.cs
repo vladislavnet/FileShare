5779241f5973a84b14085305880c61e2eb512df7
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-23 18:43:08 +0300

    Определил операции контракта

--- FileShareLib/IFileTransferService.cs
+++ FileShareLib/IFileTransferService.cs
@@ -11,6 +11,12 @@ namespace FileShareLib
     [ServiceContract]
     public interface IFileTransferService
     {
-       
-    }    
+        string UploadFolder { get; set; }
+
+        [OperationContract]
+        void UploadFile(RemoteFileInfo request);
+
+        [OperationContract]
+        RemoteFileInfo DownloadFile(DownloadRequest request);
+    }
 }
